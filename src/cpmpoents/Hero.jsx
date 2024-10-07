0c0bbb1f39ad285d3d8bfacf4793427984ba3a0c
Manish-333 <manishkumarmaurya200@gmail.com>
2024-10-07 10:33:40 +0530

    update

--- src/cpmpoents/Hero.jsx
+++ src/cpmpoents/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = ()=>{
     return (
 <div className='hero'>
-    <img src="/images/nature.png"></img>
+    <img src="/images/nn3.jpeg"></img>
 </div>
     )
 }
